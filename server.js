74b54a350f520e4fcdefb8cb8e0c50cab12661d2
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2022-04-19 16:29:09 +0000

    cm

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const upload = multer({ storage: storage });
 const app = express()
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false, limit: '100mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '1gb' }));
 
 mongoose.connect(process.env['MONGO_URI'], {
 	useNewUrlParser: true, useUnifiedTopology: true
